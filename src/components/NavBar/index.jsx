91c846ee9e25f86b18242e7310e4e65106d2b8c6
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2025-03-02 20:28:18 -0300

    refactor: adjustment in the NavBar components

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,14 +7,57 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { CartWidget } from "../CartWidget";
-import { Link } from "react-router-dom"; // Importe o Link
+import { Link } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 const navItems = [
   { name: "Shop", path: "/shop" },
   { name: "Meus Pedidos", path: "/meus-pedidos" },
 ];
 
+const categories = [
+  { name: "Bonés", path: "/category/hats" },
+  { name: "Jaquetas", path: "/category/jackets" },
+  { name: "Tênis", path: "/category/sneakers" },
+  { name: "Mulheres", path: "/category/womens" },
+  { name: "Homens", path: "/category/mens" },
+  { name: "Meias", path: "/category/socks" },
+  { name: "Mochilas", path: "/category/backpacks" },
+];
+
 export function NavBar() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [categoriesOpen, setCategoriesOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleCategoriesToggle = () => {
+    setCategoriesOpen(!categoriesOpen);
+  };
+
+  const menuContent = (
+    <>
+      {navItems.map((item) => (
+        <Button
+          key={item.name}
+          sx={{ color: "#fff" }}
+          component={Link}
+          to={item.path}
+        >
+          {item.name}
+        </Button>
+      ))}
+    </>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,31 +67,68 @@ export function NavBar() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 2, display: { xs: "block", sm: "none" } }}
+            onClick={handleDrawerToggle}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-              SportStore Ecommerce
+              SportStore
             </Link>
           </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button
-                key={item.name}
-                sx={{ color: "#fff" }}
-                component={Link}
-                to={item.path}
-              >
-                {item.name}
-              </Button>
-            ))}
-          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>{menuContent}</Box>
           <CartWidget />
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": {
+            width: 240,
+          },
+        }}
+      >
+        <List>
+          {navItems.map((item) => (
+            <ListItem
+              key={item.name}
+              component={Link}
+              to={item.path}
+              onClick={handleDrawerToggle}
+              sx={{ color: "inherit", textDecoration: "none" }}
+            >
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+          <ListItem button onClick={handleCategoriesToggle}>
+            <ListItemText primary="Categorias" />
+            {categoriesOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={categoriesOpen} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {categories.map((category) => (
+                <ListItem
+                  key={category.name}
+                  component={Link}
+                  to={category.path}
+                  onClick={handleDrawerToggle}
+                  sx={{ pl: 4, color: "inherit", textDecoration: "none" }}
+                >
+                  <ListItemText primary={category.name} />
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
+        </List>
+      </Drawer>
     </Box>
   );
 }
